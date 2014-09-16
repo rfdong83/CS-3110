74157ed7b6e4f9450c24ad6fcd3a605e9cec4c0e
Ryan <rfd83@cornell.edu>
2014-09-16 00:55:24 -0400

    ps2

--- ps2/ps2.ml
+++ ps2/ps2.ml
@@ -1,2 +1,41 @@
-let rec count_ops (tree: 'a exprTree'): int =
-	if  
+type 'a exprTree =
+| Val of 'a
+| Unop of ('a -> 'a) * 'a exprTree
+| Binop of ('a -> 'a -> 'a) * 'a exprTree * 'a exprTree
+
+
+let rec count_ops (tree: 'a exprTree): int =
+	match tree with
+    |Val _ -> 0
+    |Unop (a,b) -> 1 + count_ops(b)
+    |Binop (a,b,c) -> 1 + count_ops(b) + count_ops(c)
+
+
+let rec make_fact_tree (x: int): int exprTree =
+    if x<0 then raise (Failure "Negative number not allowed")
+        else match x with
+        |0 -> Val 1
+        |_ -> Binop(( * ), Val x, make_fact_tree(x-1))
+
+
+let rec eval (tree: 'a exprTree): 'a =
+    match tree with
+    |Val x -> x
+    |Unop (a,b) -> a (eval b)
+    |Binop (a,b,c) -> a (eval b) (eval c)
+
+
+let product (lst: float list) : float = 
+    List.fold_left ( *. ) 1. lst
+
+
+let concat_left (lst: string list) : string =
+    List.fold_left (^) "" lst
+
+
+let concat_right (lst: string list) : string =
+    List.fold_right (^) lst ""
+
+
+let mapi_lst (f : int -> 'a -> 'b) (lst: 'a list) : 'b list =
+    List.
