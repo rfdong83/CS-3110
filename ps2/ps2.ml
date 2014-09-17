bb7d7fc82fc8bacdd7e2112655b4627e42b1358f
Ryan <rfd83@cornell.edu>
2014-09-17 15:44:48 -0400

    ps2 up to show, working

--- ps2/ps2.ml
+++ ps2/ps2.ml
@@ -42,4 +42,50 @@ let mapi_lst (f : int -> 'a -> 'b) (lst: 'a list) : 'b list =
         List.fold_left(fun a _ -> a+1) 0 lst in
     match lst with
     [] -> []
-    |x::xs -> List.fold_left (fun a x -> (x+length(a))::a) [] lst 
+    |_ -> List.rev(List.fold_left (fun acc b -> 
+              (f (length(acc)) b)::acc) [] lst)
+
+
+let outline (lst: string list) : string list =
+    let outliner (x: int) (str: string) : string =
+        string_of_int (x+1) ^ ". " ^ str in
+    mapi_lst outliner lst 
+
+
+let scan_left (f: 'a -> 'b -> 'a) (acc: 'a) (lst: 'b list) : 'a list =
+    match lst with 
+    |[] -> [acc]
+    |_ -> List.rev(List.fold_left (fun a b-> 
+              (f (List.hd(a)) b)::a) [acc] lst)
+
+
+let scan_right (f: 'a -> 'b -> 'b) (lst: 'a list) (acc: 'b) : 'b list =
+    match lst with 
+    |[] -> [acc]
+    |_ -> List.rev(List.fold_right (fun a b-> 
+              (f a (List.hd(b)))::b) lst [acc])
+
+
+(* requires : n >= 1
+returns : the list [1;2;...; n] *)
+let countup ( n : int ) : int list =
+    (* tail - recursive helper function for countup :
+    accumulate the answer in l,
+    starting from n and working down *)
+    let rec countup' i l =
+        if i <=1 then l
+        else countup' (i -1) ( i :: l )
+            in countup'  n []
+
+
+let fact_list (n: int): int list =
+    scan_left ( * ) 1 (countup(n))
+
+
+let show (m: matrix): unit =
+    let print_vector(v: vector): unit =
+        List.fold_left (fun acc x -> print_string((string_of_int(x)^" ")))
+            () v in
+    List.fold_left (fun acc x -> print_vector(x)) () m 
+
+let insert_col (m: matrix) (v: vector): matrix =
