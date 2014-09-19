b6d884b17c70aa758d76fe4357992ac5f9e34f07
rfdong83 <rfd83@cornell.edu>
2014-09-18 23:42:17 -0400

    finishing ps2_test.ml

--- ps2/ps2_test.ml
+++ ps2/ps2_test.ml
@@ -3,7 +3,19 @@ open Assertions
 
 let tester (a: int) : 'b list option =
     if a mod 2 = 0 then Some [a] else None
-    
+
+let tester2 (a: 'a) : 'b list option =
+    Some [a]
+
+let tester3 (a: 'a) : 'b list option = 
+    None
+
+let tester4 (a: 'a) : 'b option =
+    Some a
+
+let tester5 (a: 'a) : 'b option =
+    if a mod 2 = 0 then Some a else None
+
 TEST_UNIT "count_ops_test1" = assert_true (count_ops (Val 2) = 0)
 TEST_UNIT "count_ops_test2" = assert_true (count_ops (Unop ((~-), Val 3)) = 1)
 TEST_UNIT "count_ops_test3" = assert_true (count_ops (Binop ((+), Val 2, Val 3)) = 1)
@@ -62,6 +74,43 @@ TEST_UNIT "add_matricies_test1" = assert_true (add_matricies [[]] [[]] = [[]])
 TEST_UNIT "add_matricies_test2" = assert_true (add_matricies [[1]] [[2]] = [[3]])
 TEST_UNIT "add_matricies_test3" = assert_true (add_matricies [[1;2;3];[4;5;6]] [[1;2;3];[4;5;6]] = [[2;4;6];[8;10;12]])
 
-TEST_UNIT "multiply_matricies_test2" = assert_true (multiply_matricies [[4]] [[2]] = [[8]])
-TEST_UNIT "multiply_matricies_test1" = assert_true (multiply_matricies [[1;2]] [[3];[4]] = [[11]])
+TEST_UNIT "multiply_matricies_test1" = assert_true (multiply_matricies [[4]] [[2]] = [[8]])
+TEST_UNIT "multiply_matricies_test2" = assert_true (multiply_matricies [[1;2]] [[3];[4]] = [[11]])
 TEST_UNIT "multiply_matricies_test3" = assert_true (multiply_matricies [[1;2;3];[4;5;6]] [[7;8];[9;10];[11;12]] = [[58;64];[139;154]])
+
+TEST_UNIT "count_wcs_test1" = assert_true (count_wcs(VarPat "hi") = 0)
+TEST_UNIT "count_wcs_test2" = assert_true (count_wcs(WCPat) = 1)
+TEST_UNIT "count_wcs_test3" = assert_true (count_wcs(TuplePat [WCPat; WCPat; WCPat; VarPat "hi"]) = 3)
+
+TEST_UNIT "count_wcs_and_var_lengths_test1" = assert_true (count_wcs_and_var_lengths (TuplePat[WCPat; WCPat; WCPat; VarPat "hi"]) = 5)
+TEST_UNIT "count_wcs_and_var_lengths_test2" = assert_true (count_wcs_and_var_lengths (TuplePat[UnitPat]) = 0)
+TEST_UNIT "count_wcs_and_var_lengths_test3" = assert_true (count_wcs_and_var_lengths (TuplePat[WCPat]) = 1)
+TEST_UNIT "count_wcs_and_var_lengths_test4" = assert_true (count_wcs_and_var_lengths (TuplePat[VarPat ""]) = 0)
+TEST_UNIT "count_wcs_and_var_lengths_test5" = assert_true (count_wcs_and_var_lengths (TuplePat[VarPat "h"]) = 1)
+TEST_UNIT "count_wcs_and_var_lengths_test6" = assert_true (count_wcs_and_var_lengths (TuplePat[WCPat; WCPat]) = 2)
+
+TEST_UNIT "count_var_test1" = assert_true (count_var "hi" (VarPat "hi") = 1 )
+TEST_UNIT "count_var_test2" = assert_true (count_var "hi" (VarPat "no") = 0 )
+TEST_UNIT "count_var_test3" = assert_true (count_var "hi" (TuplePat[VarPat "hi";VarPat "hi"; VarPat "no"]) = 2 )
+TEST_UNIT "count_var_test4" = assert_true (count_var "hi" (TuplePat[VarPat "no";VarPat "no"]) = 0 )
+
+TEST_UNIT "all_vars_unique_test1" = assert_true (all_vars_unique (VarPat "hi") = true)
+TEST_UNIT "all_vars_unique_test2" = assert_true (all_vars_unique (TuplePat[VarPat "hi";VarPat "hi"]) = false)
+TEST_UNIT "all_vars_unique_test3" = assert_true (all_vars_unique (TuplePat[VarPat "hi"; VarPat "no"]) = true)
+
+TEST_UNIT "all_answers_test1" = assert_true (all_answers tester2 [1;2;3;4;5] = Some [5;4;3;2;1] )
+TEST_UNIT "all_answers_test2" = assert_true (all_answers tester2 [] = Some [] )
+TEST_UNIT "all_answers_test3" = assert_true (all_answers tester3 ["hi" ; "boo"] = None)
+
+TEST_UNIT "match_pat_test1" = assert_true (match_pat (UnitVal, UnitPat) = Some [])
+TEST_UNIT "match_pat_test2" = assert_true (match_pat (UnitVal, VarPat "booyah") = Some ["booyah", UnitVal] )
+TEST_UNIT "match_pat_test3" = assert_true (match_pat (UnitVal, WCPat) = Some [])
+TEST_UNIT "match_pat_test4" = assert_true (match_pat (ConstVal 1, ConstPat 1) = Some [])
+TEST_UNIT "match_pat_test5" = assert_true (match_pat (ConstVal 1, UnitPat) = None)
+TEST_UNIT "match_pat_test6" = assert_true (match_pat ((TupleVal[ConstVal 1; ConstVal 2]), (TuplePat[ConstPat 1; ConstPat 2])) = Some [])
+TEST_UNIT "match_pat_test7" = assert_true (match_pat ((StructorVal("hi", (Some (TupleVal[UnitVal])) )), (StructorPat("hi" ,(Some (TuplePat[UnitPat])) ))) = Some [])
+
+TEST_UNIT "first_answer_test1" = assert_true (first_answer tester4 [1;2;3;4] = 1)
+TEST_UNIT "first_answer_test3" = assert_true (first_answer tester5 [1;2;3;4] = 2)
+
+
