3cb530eb799c12cda14fa4b2f1673fdabf62f6e4
Ryan <rfd83@cornell.edu>
2014-10-02 04:28:59 -0400

    finished up to city_search testing

---
+++ ps3/nats.ml
@@ -0,0 +1,224 @@
+(* BEGIN: DO NOT CHANGE THIS CODE, except for adding comments
+   to NATN as required in exercise 1. *)
+module type NATN = sig
+  type t
+
+  (*The additive identity: a + zero = a*)
+  val zero : t
+  (*The multiplicative indentity: one * a = a*)
+  val one : t
+  (*Addition function of natural numbers
+      Properties:
+        Commutative: a + b = b + a
+        Associative: (a + b) + c === a + (b + c)
+    Requires: 2 arguments of type t
+    Returns: the sum of the two arguments
+  *)
+  val ( + ) : t -> t -> t
+  (*Multiplication of natural numbers
+      Properties:
+        Commutative: a * b = b * a
+        Associative: (a * b) * c === a * (b * c)
+        Distributive Over Addition: a * (b + c) === (a * b) + (a * c)
+    Requires: 2 arguments of type t
+    Returns: the product of the two arguments
+  *)
+  val ( * ) : t -> t -> t 
+  (*Less-than ordering of natural numbers
+    Requires: 2 argumnts of type t
+    Returns: true if first argument is less than the second, false otherwise
+  *)
+  val ( < ) : t -> t -> bool
+  (*Equality of natural numbers
+    Requires: 2 arguments of type t
+    Returns: true if first argument is equivalent to the second, false otherwise
+  *)
+  val ( === ) : t -> t -> bool
+			    
+  exception Unrepresentable
+
+  (*Transforms natural numbers into ints
+    Requires: argument is of type t
+    Returns: int equivalent of t
+  *)
+  val int_of_nat: t -> int
+  (*Transforms ints into natural numbers
+    Requires: argument is of type int
+    Returns: natural number equivalent of int
+  *)
+  val nat_of_int: int -> t
+end
+
+module type AlienMapping = sig
+  type aliensym
+
+  val int_of_aliensym: aliensym -> int
+  val one: aliensym
+  val zero: aliensym
+end
+
+type sign = Positive | Negative
+let sign_int (n:int) : sign = 
+  if n >= 0 then Positive else Negative
+let sum_overflows (i1:int) (i2:int) : bool = 
+  sign_int i1 = sign_int i2 && sign_int(i1 + i2) <> sign_int i1
+(* END: DO NOT CHANGE THIS CODE *)
+
+(* Add your solution here for IntNat, ListNat, NatConvertFn, 
+   and AlienNatFn, being careful to use the declarations and
+   types specified in the problem set. *)
+
+module IntNat: NATN = struct
+    type t = int
+
+    exception Unrepresentable
+
+    let zero = 0
+    let one = 1
+
+    let ( + ) (t1: t) (t2: t) : t =
+        if sum_overflows t1 t2 then raise Unrepresentable else t1 + t2
+
+
+    let ( * ) (t1: t) (t2: t) : t = 
+        if ((max t1 t2) > t1*t2) 
+            || (t1*t2 = 0 && t1 <> 0 && t2 <> 0)
+            || ((t1*t2) < 0)
+        then raise Unrepresentable else t1 * t2
+
+
+    let ( === ) (t1: t) (t2: t) : bool =
+        t1 = t2
+
+ 
+    let ( < ) (t1: t) (t2: t) : bool =
+        t1 < t2  
+
+
+    let int_of_nat (num: t) : int =
+        if sum_overflows zero num then raise Unrepresentable else num
+
+
+    let nat_of_int (num: int) : t = 
+        if num < 0 then raise Unrepresentable else num
+
+end
+
+module ListNat: NATN = struct
+    (* The list [a1; ...; an] represents the
+     * natural number n. That is , the list lst represents
+     * length ( lst ). The empty list represents 0. The values of
+     * the list elements are irrelevant . *)
+    type t = int list
+
+    exception Unrepresentable
+
+    let zero = []
+    let one = [1]
+
+    let rec multiplier (count: int) (lst: int list) : int list =
+            match count with
+            | 0 -> lst
+            | _ -> multiplier (count-1) (1::lst) 
+
+    let ( + ) (t1: t) (t2: t) : t =
+        List.fold_left (fun a x -> x::a) t2 t1
+
+
+    let ( * ) (t1: t) (t2: t) : t = 
+        List.fold_left (fun a x -> multiplier (List.length t1) a) [] t2
+
+
+    let ( === ) (t1: t) (t2: t) : bool =
+        List.length t1 = List.length t2
+
+ 
+    let ( < ) (t1: t) (t2: t) : bool =
+        List.length t1 < List.length t2  
+
+
+    let int_of_nat (num: t) : int =
+        List.length num 
+
+
+    let nat_of_int (num: int) : t = 
+        multiplier num []
+
+end
+
+module NatConvertFn (N: NATN) : NATN = struct 
+    type t = N.t
+
+    exception Unrepresentable
+
+    let zero = N.zero
+    let one = N.one
+
+    let ( + ) (t1: t) (t2: t) : t =
+        N.( + ) t1 t2
+
+    let ( * ) (t1: t) (t2: t) : t =
+        N.( * ) t1 t2
+
+    let ( === ) (t1: t) (t2: t) : bool =
+        N.( === ) t1 t2
+
+    let ( < ) (t1: t) (t2: t) : bool =
+        N.( < ) t1 t2 
+
+    let int_of_nat (n: t): int = 
+        N.int_of_nat n
+
+    let nat_of_int (n: int): t =
+        N.nat_of_int n
+
+end
+
+module AlienNatFn (M : AlienMapping): NATN = struct
+    type t = M.aliensym list
+    
+    exception Unrepresentable
+
+    let zero = [M.zero]
+    let one = [M.one]
+
+    let ( + ) (t1: t) (t2: t) : t =
+        List.fold_left (fun a x -> x::a) t2 t1
+
+    let ( * ) (t1: t) (t2: t) : t =
+        let rec multiplies (num: int) (lst: t) : t list =
+            if num = 0 then [] else lst::(multiplies (num-1) lst) in
+
+        List.flatten(List.fold_left 
+            (fun a x -> List.flatten((multiplies (M.int_of_aliensym x) t1))::a)
+            [] 
+            t2
+        )
+
+    let ( === ) (t1: t) (t2: t) : bool =
+        (List.fold_left (fun a x -> if (sum_overflows (M.int_of_aliensym x) a) then 
+                                    raise Unrepresentable 
+                                    else (Pervasives.(+) (M.int_of_aliensym x)  a)) 0 t1) =
+        (List.fold_left (fun a x -> if (sum_overflows (M.int_of_aliensym x) a) then 
+                                    raise Unrepresentable 
+                                    else (Pervasives.(+) (M.int_of_aliensym x)  a)) 0 t2)
+
+    let ( < ) (t1: t) (t2: t) : bool =
+        (List.fold_left (fun a x -> if (sum_overflows (M.int_of_aliensym x) a) then 
+                                    raise Unrepresentable 
+                                    else (Pervasives.(+) (M.int_of_aliensym x)  a)) 0 t1) <
+        (List.fold_left (fun a x -> if (sum_overflows (M.int_of_aliensym x) a) then 
+                                    raise Unrepresentable 
+                                    else (Pervasives.(+) (M.int_of_aliensym x)  a)) 0 t2)
+
+    let int_of_nat (n: t) : int =
+        List.fold_left (fun a x -> if (sum_overflows (M.int_of_aliensym x)  a) then 
+                                    raise Unrepresentable 
+                                    else (Pervasives.(+) (M.int_of_aliensym x)  a)) 0 n
+
+    let nat_of_int (i: int) : t =
+        let rec adder (i: int) (accum: int) (lst: t list) : t list =
+            if accum = i then lst else adder i (Pervasives.(+) accum 1) (one::lst) in
+        List.flatten (adder i 0 [])
+ 
+end
---
+++ ps3/quadtree.ml
@@ -0,0 +1,117 @@
+(*QuadTree is a geometric representation of 2D space, allowing us to store
+    objects in this 2D space at specific locations.
+
+  coord - represents a location within a defined region, and is defined 
+    by a float pair.
+  region - represents an area, defined by a coordinate pair that label the 
+    bottom left and top right corners respectively.
+  quadtree - represents the 2D space, containing regions cut into 4 more 
+    quadtrees up until the diagonal of the region is smaller than min_diagonal
+*)
+type coord = float * float
+type region = coord * coord
+type 'a quadtree =
+  Node of region * 'a quadtree * 'a quadtree * 'a quadtree * 'a quadtree
+  | Leaf of region * ((coord * 'a) list)
+		       
+let min_diagonal = 0.0000001
+		     
+exception OutOfBounds
+
+let new_tree (r:region) : 'a quadtree = 
+    Leaf(r, [])
+        
+
+let rec insert (q: 'a quadtree) (c : coord) (s:'a) : 'a quadtree =
+    let y_midpoint (tree: 'a quadtree) : float =
+        match tree with
+        | Node (((x1,y1),(x2,y2)), q1, q2, q3, q4) -> 
+             (y1 +. y2)/.2.0
+        | Leaf(((x1,y1),(x2,y2)),_) -> 
+             (y1 +. y2)/.2.0 in
+
+    let x_midpoint (tree: 'a quadtree) : float =
+        match tree with
+        | Node (((x1,y1),(x2,y2)),q1,q2,q3,q4) ->
+            (x1 +. x2)/.2.0
+        | Leaf (((x1,y1),(x2,y2)),_) -> 
+            (x1 +. x2)/.2.0 in
+
+    let contains (r': region) (c: coord) : bool = 
+        match r' with
+        | ((x1, y1), (x2, y2)) -> 
+            fst(c) >= x1 && fst(c) < x2 && snd(c) >= y1 && snd(c) < y2 in
+
+
+    let splitter (l: 'a quadtree) : 'a quadtree =
+        match l with
+        | Leaf (((x1,y1),(x2,y2)), ((coord,obj)::t)) -> 
+             insert  
+                (Node (((x1,y1),(x2,y2)),
+                    new_tree ((x_midpoint(l), y_midpoint(l)), (x2,y2)) ,
+                    new_tree ((x1 , y_midpoint(l)) , (x_midpoint(l), y2)) , 
+                    new_tree ((x1,y1) , (x_midpoint(l), y_midpoint(l))) , 
+                    new_tree ((x_midpoint(l), y1) , (x2 , y_midpoint(l))) 
+                        )
+                    )
+                coord
+                obj
+
+        | Leaf (((x1,y1),(x2,y2)), []) -> 
+            (Node (((x1,y1),(x2,y2)),
+                    new_tree ((x_midpoint(l), y_midpoint(l)), (x2,y2)) ,
+                    new_tree ((x1 , y_midpoint(l)) , (x_midpoint(l), y2)) , 
+                    new_tree ((x1,y1) , (x_midpoint(l), y_midpoint(l))) , 
+                    new_tree ((x_midpoint(l), y1) , (x2 , y_midpoint(l))) 
+                        )
+                    )
+
+        | _ -> l in
+
+    let diagonal (r: region) : float =
+        match r with
+        | ((x1,y1),(x2,y2)) -> ((x1-.x2)**2.+. (y1-.y2)**2.)**0.5 in
+
+    let in_bounds (q: 'a quadtree) (c: coord) : bool =
+        match q with
+        | Leaf (r,_) -> (contains r c)
+        | Node (r,_,_,_,_) -> (contains r c) in 
+    
+    let rec inserter (q: 'a quadtree) (c : coord) (s:'a) : 'a quadtree =
+        match q with
+            | Leaf (r,lst) ->
+                if (contains r c) then
+                    if List.length(lst) > 0 && diagonal(r) >= min_diagonal then
+                        inserter (splitter q) c s
+                    else
+                        Leaf (r, lst @ [(c,s)])
+                else
+                    Leaf (r,lst)
+            | Node (r,t1,t2,t3,t4) ->
+                Node (r, (inserter t1 c s), (inserter t2 c s), (inserter t3 c s), (inserter t4 c s)) in
+
+    if in_bounds q c then inserter q c s else raise OutOfBounds
+
+
+let rec fold_quad (f: 'a -> (coord * 'b)  -> 'a) (a: 'a) (t: 'b quadtree): 'a =
+    match t with
+    | Leaf (r, lst) -> 
+        List.fold_left f a lst
+    | Node (r,t1,t2,t3,t4) ->
+        fold_quad f (fold_quad f (fold_quad f (fold_quad f a t1) t2) t3 ) t4
+
+   
+let rec fold_region (f: 'a -> (coord * 'b) -> 'a) (a : 'a) (t : 'b quadtree) (r : region): 'a =
+    let contains (r': region) (c: coord) : bool = 
+        match r' with
+        | ((x1, y1), (x2, y2)) -> 
+            fst(c) > x1 && fst(c) < x2 && snd(c) > y1 && snd(c) < y2 in
+    let rec scanner (r': region) (t': 'b quadtree) (accum : (coord * 'b) list ): (coord * 'b) list =
+        match t' with
+        | Leaf (r, []) -> 
+            accum
+        | Leaf (r, h::t) ->
+            List.rev (List.fold_left (fun a x-> if (contains r' (fst(x))) then  x::a else a) accum (h::t))
+        | Node (r,t1,t2,t3,t4) ->
+            scanner r' t4 (scanner r' t3 (scanner r' t2 (scanner r' t1 accum))) in
+    List.fold_left f a (scanner r t [])
